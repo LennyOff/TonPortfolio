0d5773f0eccbe0732431718539d503da04f96687
LennyOff <lenlepetitfilou@gmail.com>
2024-01-07 10:32:32 +0100

    tentative de résolution erreur 404 server (PORT 3000)

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.post("/api/sendemail", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
 });
